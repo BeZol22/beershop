8d19ba30c2ceb189b5ae8012173d3af1b5af4eef
Zoltan <zoltan.beke89@gmail.com>
2022-10-18 15:42:16 +0200

    Product tile style finished

--- src/app/components/buttons/icon-only-button/icon-only-button.component.ts
+++ src/app/components/buttons/icon-only-button/icon-only-button.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-icon-only-button',
   templateUrl: './icon-only-button.component.html',
-  styleUrls: ['./icon-only-button.component.scss']
+  styleUrls: ['./icon-only-button.component.scss'],
 })
 export class IconOnlyButtonComponent implements OnInit {
+  @Input() image = '';
+  @Input() enabled: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/product-tile/product-tile-top/product-tile-top.component.ts
+++ src/app/components/product-tile/product-tile-top/product-tile-top.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product-tile-top',
   templateUrl: './product-tile-top.component.html',
-  styleUrls: ['./product-tile-top.component.scss']
+  styleUrls: ['./product-tile-top.component.scss'],
 })
 export class ProductTileTopComponent implements OnInit {
+  buttonEnabled: boolean = true;
+  compare = 'icon-compare';
+  favorite = 'icon-make-favorite';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
